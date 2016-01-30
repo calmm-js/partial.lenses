ef198f51982a50d7be21f2c5b383766b283b12c2
Vesa Karvonen <vesa.a.j.k@gmail.com>
2016-01-30 02:01:39 +0200

    Tweaked to avoid updating equals.

--- src/partial.lenses.js
+++ src/partial.lenses.js
@@ -3,15 +3,32 @@ import R from "ramda"
 //
 
 const deleteKey = (k, o) => {
-  if (o !== undefined && k in o) {
-    const r = {}
-    for (const p in o)
-      if (p !== k)
-        r[p] = o[p]
-    return r
-  } else {
+  if (o === undefined || !(k in o))
     return o
-  }
+  const r = {}
+  for (const p in o)
+    if (p !== k)
+      r[p] = o[p]
+  return r
+}
+
+const setKey = (k, v, o) => {
+  if (o === undefined)
+    return {[k]: v}
+  if (k in o && R.equals(v, o[k]))
+    return o
+  const r = {[k]: v}
+  for (const p in o)
+    if (p !== k)
+      r[p] = o[p]
+  return r
+}
+
+//
+
+const conserve = f => (n0, o) => {
+  const n = f(n0, o)
+  return R.equals(n, o) ? o : n
 }
 
 //
@@ -24,7 +41,7 @@ const lift = l => {
   }
 }
 
-const L = (...ls) => R.compose(...ls.map(lift))
+const L = (...ls) => ls.length === 1 ? lift(ls[0]) : R.compose(...ls.map(lift))
 
 L.lens = R.lens
 L.over = R.over
@@ -33,62 +50,48 @@ L.view = R.view
 
 L.replace = (inn, out) =>
   R.lens(x => R.equals(x, inn) ? out : x,
-         y => R.equals(y, out) ? inn : y)
+         conserve(y => R.equals(y, out) ? inn : y))
 
 L.prop = k =>
-  R.lens(o => o === undefined ? undefined : o[k],
-         (v, o) => v === undefined ? deleteKey(k, o) : ({...o, [k]: v}))
+  R.lens(o => o && o[k],
+         (v, o) => v === undefined ? deleteKey(k, o) : setKey(k, v, o))
 
-L.find = predicate =>
-  R.lens(xs => xs && xs.find(predicate),
-         (x, xs) => {
-           if (x === undefined) {
-             if (xs === undefined) {
-               return undefined
-             } else {
-               const i = xs.findIndex(predicate)
-               if (i < 0)
-                 return xs
-               else
-                 return xs.slice(0, i).concat(xs.slice(i+1))
-             }
-           } else {
-             if (xs === undefined) {
-               return [x]
-             } else {
-               const i = xs.findIndex(predicate)
-               if (i < 0)
-                 return xs.concat([x])
-               else
-                 return xs.slice(0, i).concat([x], xs.slice(i+1))
-             }
-           }
-         })
+L.find = predicate => R.lens(xs => xs && xs.find(predicate), (x, xs) => {
+  if (x === undefined) {
+    if (xs === undefined)
+      return undefined
+    const i = xs.findIndex(predicate)
+    if (i < 0)
+      return xs
+    return xs.slice(0, i).concat(xs.slice(i+1))
+  } else {
+    if (xs === undefined)
+      return [x]
+    const i = xs.findIndex(predicate)
+    if (i < 0)
+      return xs.concat([x])
+    if (R.equals(x, xs[i]))
+      return xs
+    return xs.slice(0, i).concat([x], xs.slice(i+1))
+  }
+})
 
-L.index = i =>
-  R.lens(xs => xs === undefined ? undefined : xs[i],
-         (x, xs) => {
-           if (x === undefined) {
-             if (xs === undefined) {
-               return undefined
-             } else {
-               if (i < xs.length)
-                 return xs.slice(0, i).concat(xs.slice(i+1))
-               else
-                 return xs
-             }
-           } else {
-             if (xs === undefined) {
-               const ys = Array(i+1)
-               ys[i] = x
-               return ys
-             } else {
-               if (i < xs.length)
-                 return xs.slice(0, i).concat([x], xs.slice(i+1))
-               else
-                 return xs.concat(Array(i - xs.length), [x])
-             }
-           }
-         })
+L.index = i => R.lens(xs => xs && xs[i], (x, xs) => {
+  if (x === undefined) {
+    if (xs === undefined)
+      return undefined
+    if (i < xs.length)
+      return xs.slice(0, i).concat(xs.slice(i+1))
+    return xs
+  } else {
+    if (xs === undefined)
+      return Array(i).concat([x])
+    if (xs.length <= i)
+      return xs.concat(Array(i - xs.length), [x])
+    if (R.equals(x, xs[i]))
+      return xs
+    return xs.slice(0, i).concat([x], xs.slice(i+1))
+  }
+})
 
 export default L
