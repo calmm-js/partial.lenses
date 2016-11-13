b8787728d140bfb35ba124542ea700750efdaa12
Vesa Karvonen <vesa.a.j.k@gmail.com>
2016-11-13 15:32:03 +0200

    More benchmarks

--- bench/bench.js
+++ bench/bench.js
@@ -3,47 +3,62 @@ const R = require("ramda")
 
 const xyz = {x: 1, y: 2, z: 3}
 const xs = [1,2,3]
-const nested = [{x: [{y: 1}]}]
+const axay = [{x: [{y: 1}]}]
 
-const r_0 = R.lensIndex(0)
-const r_1 = R.lensIndex(1)
-const r_x = R.lensProp("x")
-const r_y = R.lensProp("y")
-const r_0_x_0_y = R.compose(r_0, r_x, r_0, r_y)
+const xyzn = {x: {y: {z: 1}}}
+
+const l_0 = R.lensIndex(0)
+const l_1 = R.lensIndex(1)
+const l_x = R.lensProp("x")
+const l_y = R.lensProp("y")
+const l_z = R.lensProp("z")
+const l_0_x_0_y = R.compose(l_0, l_x, l_0, l_y)
+const l_xyz = R.lensPath(["x", "y", "z"])
+const l_x_y_z = R.compose(l_x, l_y, l_z)
 
 const inc = x => x + 1
 
 const bs = [
   'L.get(1, xs)',
   'R.nth(1, xs)',
-  'R.view(r_1, xs)',
+  'R.view(l_1, xs)',
 
   'L.set(1, 0, xs)',
   'R.update(1, 0, xs)',
-  'R.set(r_1, 0, xs)',
+  'R.set(l_1, 0, xs)',
 
   'L.get("y", xyz)',
   'R.prop("y", xyz)',
-  'R.view(r_y, xyz)',
+  'R.view(l_y, xyz)',
 
   'L.set("y", 0, xyz)',
   'R.assoc("y", 0, xyz)',
-  'R.set(r_y, 0, xyz)',
+  'R.set(l_y, 0, xyz)',
 
-  'L.get([0, "x", 0, "y"], nested)',
-  'R.view(r_0_x_0_y, nested)',
+  'L.get([0,"x",0,"y"], axay)',
+  'R.view(l_0_x_0_y, axay)',
 
-  'L.set([0, "x", 0, "y"], 0, nested)',
-  'R.set(r_0_x_0_y, 0, nested)',
+  'L.set([0,"x",0,"y"], 0, axay)',
+  'R.set(l_0_x_0_y, 0, axay)',
 
-  'L.modify([0, "x", 0, "y"], inc, nested)',
-  'R.over(r_0_x_0_y, inc, nested)',
+  'L.modify([0,"x",0,"y"], inc, axay)',
+  'R.over(l_0_x_0_y, inc, axay)',
 
   'L.remove(1, xs)',
   'R.remove(1, 1, xs)',
 
   'L.remove("y", xyz)',
   'R.dissoc("y", xyz)',
+
+  'L.get(["x","y","z"], xyzn)',
+  'R.path(["x","y","z"], xyzn)',
+  'R.view(l_xyz, xyzn)',
+  'R.view(l_x_y_z, xyzn)',
+
+  'L.set(["x","y","z"], 0, xyzn)',
+  'R.assocPath(["x","y","z"], 0, xyzn)',
+  'R.set(l_xyz, 0, xyzn)',
+  'R.set(l_x_y_z, 0, xyzn)',
 
   'L.get(L.defaults(1), undefined)',
   'L.get(L.defaults(1), 2)',
