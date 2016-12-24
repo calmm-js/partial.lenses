3c755bd55ce89e32361a577e6ab20e23381964de
Vesa Karvonen <vesa.a.j.k@gmail.com>
2016-12-24 16:49:48 +0900

    Short lines

--- test/core.js
+++ test/core.js
@@ -16,8 +16,12 @@ export const set = R.curry((o, x, s) => modify(o, R.always(x), s))
 
 export const compose = L.compose
 
-export const chain = R.curry((x2yO, xO) => [xO, choose((xM, i) => isDefined(xM) ? x2yO(xM, i) : zero)])
-export const choice = (...ls) => choose(x => {const i = ls.findIndex(l => isDefined(get(l, x))); return 0 <= i ? ls[i] : zero})
+export const chain = R.curry((x2yO, xO) =>
+  [xO, choose((xM, i) => isDefined(xM) ? x2yO(xM, i) : zero)])
+export const choice = (...ls) => choose(x => {
+  const i = ls.findIndex(l => isDefined(get(l, x)))
+  return 0 <= i ? ls[i] : zero
+})
 export const choose = L.choose
 export const when = p => choose((x, i) => p(x, i) ? identity : zero)
 export const zero = L.zero
@@ -29,7 +33,8 @@ export const log = L.log
 // Traversals
 
 export const collect = R.curry((t, s) => collectMap(t, R.identity, s))
-export const collectMap = R.curry((t, to, s) => foldMapOf(Collect, t, R.pipe(to, toCollect), s))
+export const collectMap = R.curry((t, to, s) =>
+  foldMapOf(Collect, t, R.pipe(to, toCollect), s))
 export const foldMapOf = L.foldMapOf
 
 export const branch = L.branch
@@ -47,28 +52,42 @@ export const augment = L.augment
 
 export const defaults = v => replace(undefined, v)
 export const define = v => [required(v), defaults(v)]
-export const normalize = xi2x => lens((x, i) => isDefined(x) ? xi2x(x, i) : x, (x, _, i) => isDefined(x) ? xi2x(x, i) : x)
+export const normalize = xi2x =>
+  lens((x, i) => isDefined(x) ? xi2x(x, i) : x,
+       (x, _, i) => isDefined(x) ? xi2x(x, i) : x)
 export const required = v => replace(v, undefined)
-export const rewrite = xi2x => lens(R.identity, (x, _, i) => isDefined(x) ? xi2x(x, i) : x)
+export const rewrite = xi2x =>
+  lens(R.identity, (x, _, i) => isDefined(x) ? xi2x(x, i) : x)
 
 export const append = choose(s => R.is(Array, s) ? s.length : 0)
 export const filter = L.filter
-export const find = p => choose(xs => {if (!R.is(Array, xs)) return append; const i = xs.findIndex((x, i) => p(x, i)); return i < 0 ? append : i})
-export const findWith = (...ls) => {const lls = compose(...ls); return [find(x => isDefined(get(lls, x))), lls]}
+export const find = p => choose(xs => {
+  if (!R.is(Array, xs))
+    return append
+  const i = xs.findIndex((x, i) => p(x, i))
+  return i < 0 ? append : i
+})
+export const findWith = (...ls) => {
+  const lls = compose(...ls)
+  return [find(x => isDefined(get(lls, x))), lls]
+}
 export const index = L.index
 
 export const prop = L.prop
 export const props = (...ps) => pick(R.zipObj(ps, ps))
 
-export const valueOr = v => lens(s => s === null || s === undefined ? v : s, R.identity)
+export const valueOr = v =>
+  lens(s => s === null || s === undefined ? v : s, R.identity)
 
-export const orElse = R.curry((d, l) => choose(x => isDefined(get(l, x)) ? l : d))
+export const orElse = R.curry((d, l) =>
+  choose(x => isDefined(get(l, x)) ? l : d))
 
 export const just = v => to(R.always(v))
 export const to = a2b => lens(a2b, (_, s) => s)
 
 export const pick = L.pick
-export const replace = R.curry((i, o) => iso(x => R.equals(i, x) ? o : x, x => R.equals(o, x) ? i : x))
+export const replace = R.curry((i, o) =>
+  iso(x => R.equals(i, x) ? o : x, x => R.equals(o, x) ? i : x))
 
 // Isomorphisms
 
