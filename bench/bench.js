ced1e9e7715834cbc3829a838f0c1abc5053f0f8
Vesa Karvonen <vesa.a.j.k@gmail.com>
2016-12-29 09:37:48 +0900

    New comparative benches

--- bench/bench.js
+++ bench/bench.js
@@ -4,6 +4,7 @@ const I = require("infestines")
 const L = require("../lib/partial.lenses")
 const P = require("ramda-lens")
 const R = require("ramda")
+const O = require("flunc-optics")
 
 const xyz = {x: 1, y: 2, z: 3}
 const xs = [1,2,3]
@@ -24,8 +25,16 @@ const l_x = R.lensProp("x")
 const l_y = R.lensProp("y")
 const l_z = R.lensProp("z")
 const l_0_x_0_y = R.compose(l_0, l_x, l_0, l_y)
+const l_0x0y = R.lensPath([0, "x", 0, "y"])
 const l_xyz = R.lensPath(["x", "y", "z"])
 const l_x_y_z = R.compose(l_x, l_y, l_z)
+
+const o_x_y_z = R.compose(O.Lens.atObject('x'),
+                          O.Prism._Just,
+                          O.Lens.atObject('y'),
+                          O.Prism._Just,
+                          O.Lens.atObject('z'),
+                          O.Prism._Just)
 
 const id = I.id
 const always = I.always
@@ -77,22 +86,20 @@ const values = L.lazy(rec => [
 const bstPairs = [[3, "g"], [2, "a"], [1, "m"], [4, "i"], [5, "c"]]
 const bst = fromPairs(bstPairs)
 
-
 const bs = [
-  'L.modify(values, x => x + x, bst)',
-  'L.collect(values, bst)',
-
-  'fromPairs(bstPairs)',
-
   'L.foldMapOf(Sum, L.sequence, id, xs100)',
   'P.sumOf(P.traversed, xs100)',
   'R.sum(xs100)',
+  'O.Fold.sumOf(O.Traversal.traversed, xs100)',
 
   'L.collect(L.sequence, xs100)',
+  'O.Fold.toListOf(O.Traversal.traversed, xs100)',
 
   'L.modify(L.sequence, inc, xs100)',
   'P.over(P.traversed, inc, xs100)',
   'R.map(inc, xs100)',
+  'O.Setter.over(O.Traversal.traversed, inc, xs100)',
+
   'L.get(1, xs)',
   'R.nth(1, xs)',
   'R.view(l_1, xs)',
@@ -110,12 +117,17 @@ const bs = [
   'R.set(l_y, 0, xyz)',
 
   'L.get([0,"x",0,"y"], axay)',
+  'R.path([0,"x",0,"y"], axay)',
+  'R.view(l_0x0y, axay)',
   'R.view(l_0_x_0_y, axay)',
 
   'L.set([0,"x",0,"y"], 0, axay)',
+  'R.assocPath([0,"x",0,"y"], 0, axay)',
+  'R.set(l_0x0y, 0, axay)',
   'R.set(l_0_x_0_y, 0, axay)',
 
   'L.modify([0,"x",0,"y"], inc, axay)',
+  'R.over(l_0x0y, inc, axay)',
   'R.over(l_0_x_0_y, inc, axay)',
 
   'L.remove(1, xs)',
@@ -128,11 +140,13 @@ const bs = [
   'R.path(["x","y","z"], xyzn)',
   'R.view(l_xyz, xyzn)',
   'R.view(l_x_y_z, xyzn)',
+  'O.Getter.view(o_x_y_z, xyzn)',
 
   'L.set(["x","y","z"], 0, xyzn)',
   'R.assocPath(["x","y","z"], 0, xyzn)',
   'R.set(l_xyz, 0, xyzn)',
   'R.set(l_x_y_z, 0, xyzn)',
+  'O.Setter.set(o_x_y_z, 0, xyzn)',
 
   'L.remove(50, xs100)',
   'R.remove(50, 1, xs100)',
@@ -150,6 +164,11 @@ const bs = [
   'L.set(5000, 2, xs10000)',
   'R.remove(5000, 1, xs10000)',
   'R.update(5000, 2, xs10000)',
+
+  'L.modify(values, x => x + x, bst)',
+  'L.collect(values, bst)',
+
+  'fromPairs(bstPairs)',
 
   'L.get(L.defaults(1), undefined)',
   'L.get(L.defaults(1), 2)',
