ad07ea6f70f48cb1e86d9c87a2ee5a7c5ba41530
Vesa Karvonen <vesa.a.j.k@gmail.com>
2017-01-08 23:35:47 +0200

    More balanced spacing

--- bench/bench.js
+++ bench/bench.js
@@ -263,7 +263,7 @@ function complete() {
                         R.map(R.prop("hz")),
                         R.reduce(R.max, 0))
   bs.forEach(b => {
-    console.log(sprintf('%12s/s %8.2fx  %s',
+    console.log(sprintf('%12s/s %8.2fx   %s',
                         Math.round(b.hz).toLocaleString(),
                         fastest/b.hz, b.name))
   })
