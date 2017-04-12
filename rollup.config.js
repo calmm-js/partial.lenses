9df12f067ed5c80850f6ac923d50348e4729d825
Vesa Karvonen <vesa.a.j.k@gmail.com>
2017-04-13 02:18:28 +0300

    Simplify

--- rollup.config.js
+++ rollup.config.js
@@ -6,14 +6,12 @@ import uglify      from "rollup-plugin-uglify"
 export default {
   external: ["infestines"],
   globals: {"infestines": "I"},
-  plugins: [].concat(
-    process.env.NODE_ENV
-    ? [replace({"process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV)})]
-    : [],
-    [nodeResolve({modulesOnly: true})],
-    [babel()],
-    process.env.NODE_ENV === "production"
-    ? [uglify()]
-    : []
-  )
+  plugins: [
+    process.env.NODE_ENV &&
+      replace({"process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV)}),
+    nodeResolve({modulesOnly: true}),
+    babel(),
+    process.env.NODE_ENV === "production" &&
+      uglify()
+  ].filter(x => x)
 }
