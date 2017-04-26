7866342ccf24741a6c3232672b548ee94830cc9a
Vesa Karvonen <vesa.a.j.k@gmail.com>
2017-04-26 17:14:23 +0300

    Copy code to title

--- docs/start.js
+++ docs/start.js
@@ -16,17 +16,26 @@
     var pg = document.getElementById("playground")
     if (pg) {
       var hash = window.location.hash.slice(1)
-      if (hash)
-        pg.innerHTML = window.LZString.decompressFromEncodedURIComponent(hash)
+      function updateTitle(text) {
+        document.title =
+          "Partial Lenses Playground: " + text.replace(/\s+/g, " ").trim()
+      }
+      if (hash) {
+        var text = window.LZString.decompressFromEncodedURIComponent(hash)
+        pg.innerHTML = text
+        updateTitle(text)
+      }
       var to = null
       document.onkeyup = function () {
         clearTimeout(to)
         to = setTimeout(function () {
           var elem = document.querySelector(".CodeMirror-lines")
-          if (elem)
+          if (elem) {
             window.location.hash =
               window.LZString.compressToEncodedURIComponent(
                 elem.innerText.replace(/\u200B|^\s*|\s*$/gm, ""))
+            updateTitle(elem.innerText)
+          }
         }, 250)
       }
     }
