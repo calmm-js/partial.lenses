6370ec2c466aaa6149c98bfe59dd04c126e24182
Vesa Karvonen <vesa.a.j.k@gmail.com>
2018-02-08 20:43:04 +0200

    Tweaked Uglify options

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,26 @@
-import babel       from "rollup-plugin-babel"
-import nodeResolve from "rollup-plugin-node-resolve"
-import replace     from "rollup-plugin-replace"
-import uglify      from "rollup-plugin-uglify"
+import babel from 'rollup-plugin-babel'
+import nodeResolve from 'rollup-plugin-node-resolve'
+import replace from 'rollup-plugin-replace'
+import uglify from 'rollup-plugin-uglify'
 
 export default {
-  external: ["infestines"],
+  external: ['infestines'],
   output: {
-    globals: {"infestines": "I"}
+    globals: {infestines: 'I'}
   },
   plugins: [
     process.env.NODE_ENV &&
-      replace({"process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV)}),
+      replace({'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)}),
     nodeResolve({modulesOnly: true}),
     babel(),
-    process.env.NODE_ENV === "production" &&
-      uglify()
+    process.env.NODE_ENV === 'production' &&
+      uglify({
+        compress: {
+          hoist_funs: true,
+          passes: 3,
+          pure_getters: true,
+          pure_funcs: ['require']
+        }
+      })
   ].filter(x => x)
 }
