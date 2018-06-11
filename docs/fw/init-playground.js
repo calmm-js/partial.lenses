0780fefaae71a83b534d39ec17fe984bd06d7791
Vesa Karvonen <vesa.a.j.k@gmail.com>
2018-06-11 16:32:09 +0300

    Hacky playground highlight support

--- docs/fw/init-playground.js
+++ docs/fw/init-playground.js
@@ -9,6 +9,8 @@
       cursorBlinkRate: 0
     }
 
+    var highlightedLines = {}
+
     var hash = window.location.hash.slice(1)
     function updateTitle(text) {
       document.title = document.title.replace(
@@ -21,30 +23,103 @@
       )
     }
     if (hash) {
-      var text = window.LZString.decompressFromEncodedURIComponent(hash)
-      pg.innerHTML = text.replace(/[\u00A0-\u9999<>&]/gim, function(i) {
-        return '&#' + i.charCodeAt(0) + ';'
-      })
-      updateTitle(text)
+      var params = hash.split('&')
+      if (params.length) {
+        var text = window.LZString.decompressFromEncodedURIComponent(params[0])
+        pg.innerHTML = text.replace(/[\u00A0-\u9999<>&]/gim, function(i) {
+          return '&#' + i.charCodeAt(0) + ';'
+        })
+        updateTitle(text)
+        for (var i = 1; i < params.length; ++i) {
+          if (params[i][0] === 'L') {
+            var linum = Number(params[i].slice(1))
+            if (0 <= linum) {
+              highlightedLines[linum] = true
+            }
+          }
+        }
+      }
     }
+
+    function getCodeMirror() {
+      var elem = document.querySelector('.CodeMirror.cm-s-default')
+      return elem && elem.CodeMirror
+    }
+
+    var highlight = 'background: #fffbdd;'
+    function highlightLines() {
+      Array.from(
+        document.querySelectorAll('.klipse-snippet .CodeMirror-line')
+      ).forEach(function(line, indexMinus1) {
+        if (highlightedLines[indexMinus1 + 1]) {
+          line.style.cssText = highlight
+        } else {
+          line.style.cssText = ''
+        }
+      })
+    }
+    function initialHighlight() {
+      const cm = getCodeMirror()
+      if (
+        cm &&
+        document.querySelectorAll('.klipse-snippet .CodeMirror-line').length
+      ) {
+        cm.on('change', function() {
+          highlightLines((highlightedLines = {}))
+        })
+        highlightLines()
+      } else {
+        setTimeout(initialHighlight, 100)
+      }
+    }
+    initialHighlight()
+
+    document.onmousedown = function(e) {
+      var target = e.target
+      if (target.className === 'CodeMirror-linenumber CodeMirror-gutter-elt') {
+        const linum = Number(target.textContent)
+        if (0 <= linum) {
+          if (highlightedLines[linum]) {
+            delete highlightedLines[linum]
+          } else {
+            highlightedLines[linum] = true
+          }
+          highlightLines()
+          updateLocationAndTitle()
+        }
+      }
+    }
+
+    function updateLocationAndTitle() {
+      var cm = getCodeMirror()
+      if (cm) {
+        var text = cm.getValue()
+        var compressedText = window.LZString.compressToEncodedURIComponent(
+          text.replace(/[ \t]*[\n][ \t]*/g, '\n')
+        )
+        var lines = Object.keys(highlightedLines)
+          .map(function(l) {
+            return 'L' + l
+          })
+          .join('&')
+        history.replaceState(
+          null,
+          '',
+          window.location.pathname +
+            '#' +
+            compressedText +
+            (lines ? '&' + lines : '')
+        )
+        updateTitle(text)
+      }
+    }
+
     var to = null
     document.onkeyup = function() {
       clearTimeout(to)
       to = setTimeout(function() {
-        var cm = document.querySelector('.CodeMirror.cm-s-default').CodeMirror
-        if (cm) {
-          var text = cm.getValue()
-          history.replaceState(
-            null,
-            '',
-            window.location.pathname +
-              '#' +
-              window.LZString.compressToEncodedURIComponent(
-                text.replace(/[ \t]*[\n][ \t]*/g, '\n')
-              )
-          )
-          updateTitle(text)
-        }
+        highlightLines()
+        updateLocationAndTitle()
       }, 250)
     }
 
